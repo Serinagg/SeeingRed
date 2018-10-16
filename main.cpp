49b7d1dbc5414eb2d2243773d6f0da6026f67fb8
serinagg <sgarcia14@mail.csuchico.edu>
2018-10-15 17:59:38 -0700

    finding height and width of images

--- main.cpp
+++ main.cpp
@@ -8,6 +8,26 @@ using namespace std;
 //argv - each argument word, stored in an array
 int main(int argc, char* argv[])
 {
-  
+        if(argc == 2) 
+        {
+           string filename;
+           filename = argv[1];
+           Bitmap image;
+           image.open(filename);
+           if (image.isImage()) 
+           {
+                  vector<vector<Pixel> >imagePixels = image.toPixelMatrix();
+
+                  cout<<filename<<" is "<<imagePixels.size()<<" pixels high and "<<imagePixels[0].size()<<" pixels wide\n";
+
+           } else {
+                   cout<<"Image file must be a bitmap with 24-bit color depth.\n";
+           }
+
+
+        } else { 
+                cout<<"Please specify one image file!\n";
+
+        }
   return 0;
 }
